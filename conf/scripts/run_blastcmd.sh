477483689e10fa857a5a2baf1226873bd2883310
Patrick DURAND <patrick@plealog.com>
2022-11-10 10:20:25 +0000

    update run_blastcmd to use new scheduler API

--- conf/scripts/run_blastcmd.sh
+++ conf/scripts/run_blastcmd.sh
@@ -1,83 +1,67 @@
 #!/usr/bin/env bash
 
+# TO TEST:
+# /appli/bioinfo/beedeem/conf/scripts/run_blastcmd.sh -w /home/datawork-bioinfo-ss/beedeem/ -d /home/datawork-bioinfo-ss/beedeem/test-scheduler-scripts/p/PDB_proteins/current/PDB_proteins -n PDB_proteins -t p -p ifremer
+
+
+
 # This is a BeeDeeM external task script aims at running
 # a Blastdbcmd job. 
 #
 # Such a BeeDeeM script is called by the task engine and
-# with these arguments: -w <path> -d <path> -f <path> -n <name> -t <type>
-#
-#  -w <path>: <path> is the working directory path.
-#             provided for both unit and global tasks.
-#  -d <path>: <path> is the bank installation path.
-#             provided for both unit and global tasks.
-#  -f <path>: <path> is the path to file under unit task processing
-#             only provided with unit task.
-#  -n <name>: <name> is the bank name.
-#  -t <type>: <path> is the bank type. One of p, n or d.
-#             p: protein
-#             n: nucleotide
-#             d: dictionary or ontology
+# with arguments as defined in: 
+# ./scheduler/common.sh->handleBDMArgs() function
 
-echo "----"
 echo "Make a FASTA file out of a BLAST bank (blastdbcmd)"
-echo "Arguments coming from BeeDeeM are:"
-echo $@
-echo "----"
-# Prepare arguments for processing
-WK_DIR=
-INST_DIR=
-PROCESSED_FILE=
-BANK_NAME=
-BANK_TYPE=
-while getopts w:d:f:n:t: opt
-do
-    case "$opt" in
-      w)  WK_DIR="$OPTARG";;
-      d)  INST_DIR="$OPTARG";;
-      f)  PROCESSED_FILE="$OPTARG";;
-      n)  BANK_NAME="$OPTARG";;
-      t)  BANK_TYPE="$OPTARG";;
-    esac
-done
-shift `expr $OPTIND - 1`
-# remaining arguments, if any, are stored here
-MORE_ARGS=$@
-
-echo "Working dir: $WK_DIR"
-echo "Install dir: $INST_DIR"
-echo "Processed file: $PROCESSED_FILE"
-echo "Bank name: $BANK_NAME"
-echo "Bank type: $BANK_TYPE"
-echo "----"
 
 # ========================================================================================
 # Section: include API
-script_dir=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
-. $script_dir/scheduler/pbs_wrapper.sh
-
-# ========================================================================================
-# Section: submit job to scheduler
-MY_SCRIPT="$script_dir/run_blastcmd.pbs -d $INST_DIR -n $BANK_NAME "
-echo "Submitting ${MY_SCRIPT}..."
-ANSWER=$(submitEx "sequentiel" "8g" "1" "12:00:00" "blastcmd-$BANK_NAME" "$WK_DIR" "-- ${MY_SCRIPT}")
-RET_CODE=$?
-if [ ! $RET_CODE -eq 0 ]; then 
-  echo $ANSWER
+S_NAME=$(realpath "$0")
+script_dir=$(dirname "$S_NAME")
+. $script_dir/scheduler/common.sh 
+JOB_SCHEDULER=$(getScheduler)
+if [ $? -eq 0 ]; then
+  . $script_dir/scheduler/${JOB_SCHEDULER}_wrapper.sh
+else 
+  errorMsg "No Job Scheduler found" 
   exit 1
 fi
-JOB_ID=$ANSWER
-echo "Job ID is: $JOB_ID"
-echo "Waiting for job to complete..."
-waitForJobToFinish $JOB_ID 
-if [ ! $RET_CODE -eq 0 ]; then
-  echo "FAILED"
-  RET_CODE=1
-else
-  echo "SUCCESS"
-  RET_CODE=0
-fi
-echo "Job log is:"
-dumpJobLog $WK_DIR $JOB_ID
 
-exit $RET_CODE
+# ========================================================================================
+# Section: handle arguemnts
+# Function call sets BDMC_xxx variables from cmdline arguments
+handleBDMArgs $@
+RET_CODE=$?
+[ ! $RET_CODE -eq 0 ] && errorMsg "Wrong or missing arguments" && exit $RET_CODE
+
+# ========================================================================================
+# Section: do business
+# Get job scheduler resource file using canonical naming
+CFG_SCHEDULER="$script_dir/scheduler/${JOB_SCHEDULER}_${BDMC_PLATFORM}.cfg"
+CFG_SOFT="blastcmd"
+# Get all resources at one as a string
+CFG_RESOURCES=$(getResources $CFG_SCHEDULER $CFG_SOFT)
+RET_CODE=$?
+[ ! $RET_CODE -eq 0 ] && errorMsg "Unable to get resources to submit job" && exit $RET_CODE
+# Convert that string as an array
+CFG_RESOURCES=($CFG_RESOURCES)
+# Go!
+MY_SCRIPT="$script_dir/run_$CFG_SOFT.job -d $BDMC_INST_DIR -n $BDMC_BANK_NAME -p $BDMC_PLATFORM "
+echo "> Submitting ${MY_SCRIPT}..."
+ANSWER=$(submitEx "${CFG_RESOURCES[3]}" "${CFG_RESOURCES[1]}" "${CFG_RESOURCES[0]}" "${CFG_RESOURCES[2]}" "$CFG_SOFT-$BDMC_BANK_NAME" "$BDMC_WK_DIR" "${MY_SCRIPT}")
+RCODE=$?
+[ ! $RCODE -eq 0 ] && echo $ANSWER && exit $RCODE
+JOB_ID=$ANSWER
+echo "> Job ID is: $JOB_ID"
+echo "> Waiting for job to complete..."
+ANSWER=$(waitForJobToFinish $JOB_ID)
+RCODE=$?
+[ $RCODE -eq 0 ] && echo "SUCCESS" || echo "$ANSWER \nFAILED: $RCODE"
+echo "> Begin job log:"
+dumpJobLog $BDMC_WK_DIR $JOB_ID
+echo "< End Job log"
+removeJobLog $BDMC_WK_DIR $JOB_ID
+exit $RCODE
+
+
 
