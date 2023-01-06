42bb82c42f0febf918fc8fee252d63d8f02cb004
pgdurand <patrick@plealog.com>
2023-01-06 15:05:03 +0100

    update legacy installer and test script

--- scripts/test_bdm.sh
+++ scripts/test_bdm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # =================================================
-# Test script for BeeDeeM legacy installer.
+# Test script for BeeDeeM legacy installer or Conda.
 # To be used directly on the command-line.
 # DO NOT use for Singularity/Docker installations.
 #
@@ -10,21 +10,28 @@
 # Step 1: install BeeDeeM as stated at:
 #         https://pgdurand.gitbook.io/beedeem/installation/installation#legacy-installation 
 #
-# Step 2: run test as follows: 
-#         ./test_bdm.sh
+# Step 2: run test as follows:
+#         cd /path/to/beedeem 
+#         export PATH=$PWD:$PATH
+#         ./test_bdm
 # --
 # P. Durand (SeBiMER, Ifremer), last updated on Jan 2023
 # =================================================
 
+
 # == TEST 1 =========================================== 
 echo "*** TEST 1: Start simple bank installation"
-# set log mode to console (otherwise default is a log file located in KL_WORKING_DIR
+
+# set BeeDeeM log mode to console (otherwise default is a log file located in KL_WORKING_DIR)
 export KL_LOG_TYPE=console
+
 # Set the bank to install
 # This is a '.dsc' file located in BeeDeeM installation path at ${beedeem-home}/conf/descriptors
 DESCRIPTOR="SwissProt_human"
+
 # start BeeDeeM with 'install' command
-./bdm install -desc ${DESCRIPTOR} 
+bdm install -desc ${DESCRIPTOR} 
+
 # check whether command succeeded or not
 if [ $? -eq 0 ]; then
   echo "TEST 1: SUCCESS"
@@ -33,12 +40,16 @@ else
   exit 1
 fi
 
+
 # == TEST 2 =========================================== 
 echo "*** TEST 2: list installed bank"
-# reset log mode to file
+
+# reset BeeDeeM log mode to file (default)
 export KL_LOG_TYPE=file
+
 # start BeeDeeM with 'info' command
-./bdm info -d all -f txt
+bdm info -d all -f txt
+
 if [ $? -eq 0 ]; then
   echo "TEST 2: SUCCESS"
 else
@@ -46,13 +57,17 @@ else
   exit 1
 fi
 
+
 # == TEST 3 =========================================== 
-# Change default log file name to something else
+# Change default BeeDeeM log file name to something else
 export KL_LOG_FILE=query.log
+
 SW_ENTRY="ZZZ3_HUMAN"
 echo "*** TEST 3: query bank for entry: $SW_ENTRY"
+
 # start BeeDeeM with 'query' command
-./bdm query -d protein -f txt -i $SW_ENTRY
+bdm query -d protein -f txt -i $SW_ENTRY
+
 if [ $? -eq 0 ]; then
   echo "TEST 3: SUCCESS"
 else
